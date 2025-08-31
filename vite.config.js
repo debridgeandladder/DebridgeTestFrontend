301d1ca0c861b28b057538b3d256bde9f7756512
Lateef Yusuf <49310610+yusuflateef@users.noreply.github.com>
2025-09-01 00:02:31 +0100

    upy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./dist"),
+      "@": path.resolve(__dirname, "./src"),
     },
     base:"DebridgeTestFrontend"
   },
