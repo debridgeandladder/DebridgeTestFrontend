df2a80197535df1cc80b9796bef5ac84c371d20f
Lateef Yusuf <49310610+yusuflateef@users.noreply.github.com>
2025-08-31 21:20:14 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./dist/asset"),
+      "@": path.resolve(__dirname, "./src"),
     },
+    base:"DebridgeTestFrontend"
   },
 })
